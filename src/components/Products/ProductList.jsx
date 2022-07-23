2a1c33b5efe47095b95f6551b6675ed9397331f5
debz1 <debenshakya1@gmail.com>
2022-07-23 14:48:46 +0545

    using context to show product

--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -1,36 +1,9 @@
 import React from "react";
+import { useProduct } from "../context/Product";
 import ProductCard from "./ProductCard";
 
 const ProductList = () => {
-  const products = [
-    {
-      id: 1,
-      quantity: 5,
-      name: "pizza",
-      price: 120,
-      description: "pizza is good",
-      imageURL:
-        "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg",
-    },
-    {
-      id: 2,
-      quantity: 5,
-      name: "pizza",
-      price: 160,
-      description: "pizza is good",
-      imageURL:
-        "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg",
-    },
-    {
-      id: 3,
-      quantity: 5,
-      name: "pizza",
-      price: 180,
-      description: "pizza is good",
-      imageURL:
-        "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg",
-    },
-  ];
+const {products}=useProduct()
   return (
     <div className="col-span-2 flex flex-wrap">
       {products.map((product) => (
