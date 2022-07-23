39fc7ce34dcf17d29870ce162cb20d3fac12d1cf
debz1 <debenshakya1@gmail.com>
2022-07-23 07:34:11 +0545

    add total price

--- src/components/Billing/Orders.jsx
+++ src/components/Billing/Orders.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
-import Order from './Order'
+import React from "react";
+import Order from "./Order";
 
-const Orders = ({orders}) => {
+const Orders = ({ orders }) => {
+  const getTotalAmount = () => {
+    return orders.reduce((x, y) => {
+      return x = x + y.price * y.quantity;
+    }, 0);
+  };
   return (
     <div className="h-[500px]">
-        <table className="table w-full">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>quantity</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {orders && orders.map((order)=>(
-             <Order order={order}/>
-            ))}
-          </tbody>
-        </table>
-        <hr />
-        <div className="flex justify-between px-5">
-          <div>Total</div>
-          <div>2000</div>
-        </div>
-        <hr />
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders && orders.map((order) => <Order order={order} />)}
+        </tbody>
+      </table>
+      <hr />
+      <div className="flex justify-between px-5">
+        <div>Total</div>
+        <div>{getTotalAmount()}</div>
       </div>
-  )
-}
+      <hr />
+    </div>
+  );
+};
 
-export default Orders
+export default Orders;
