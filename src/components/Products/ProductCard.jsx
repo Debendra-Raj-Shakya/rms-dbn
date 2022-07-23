2d8cc85a089c0977b0e3fbc11cdcc923643dd569
debz1 <debenshakya1@gmail.com>
2022-07-23 16:47:01 +0545

    adding modal (pop up) in productList

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 const ProductCard = ({
   product: { id, imageURL, name, price, description },
 }) => {
+  const [modal, setModal] = useState(false);
   return (
     <div key={id} className="card w-96 bg-base-100 shadow-xl h-[28rem]">
       <figure className="px-10 pt-10">
@@ -12,7 +13,61 @@ const ProductCard = ({
         <h2 className="card-title">{name}</h2>
         <p>{description}</p>
         <div className="card-actions">
-          <button className="btn btn-primary">Buy Now/Order {price}</button>
+          <input
+            type="checkbox"
+            checked={modal}
+            id="add_product-modal"
+            className="modal-toggle"
+          />
+          <div className="modal">
+            <div className="modal-box relative">
+              <button
+                onClick={() => setModal(false)}
+                className="btn btn-sm btn-circle absolute right-2 top-2"
+              >
+                ✕
+              </button>
+              {/* quantity orders start */}
+              <Formik initialValues={{quantity:1}} onSubmit={()=>{}}>
+                {() => (
+                  <Form>
+                    <div className="form-control w-full max-w-xs">
+                      <label className="label">
+                        <span className="label-text">Product Quantity</span>
+                      </label>
+                      <Field
+                        name="quantity"
+                        // value={values.quantity}
+                        // onChange={handleChange}
+                        type="number"
+                        placeholder="Type quantity of food"
+                        className="input input-bordered w-full max-w-lg"
+                      />
+                      <label className="label">
+                        <ErrorMessage
+                          name="quantity"
+                          component="span"
+                          className="label-text-alt"
+                        />
+                      </label>
+                    </div>
+                  </Form>
+                )}
+              </Formik>
+
+              {/* quantity orders end */}
+              <div className="modal-action">
+                {/* <label htmlFor="add_product-modal" className="btn">
+                Add Product in store
+              </label> */}
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-end mx-10 my-6">
+            <button onClick={() => setModal(true)} className="btn btn-primary">
+              add products
+            </button>
+          </div>
         </div>
       </div>
     </div>
