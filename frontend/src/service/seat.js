e7d93a448a16ddab2778639488d9e8dd1ad364f0
debz1 <debenshakya1@gmail.com>
2022-08-04 18:58:27 +0545

    adding service folder and linking to backend

---
+++ frontend/src/service/seat.js
@@ -0,0 +1,17 @@
+import api from "../utils/interceptor";
+
+export const getSeat = async () => {
+  return api.get("/seat");
+};
+
+export const getOrder = async (deskId) => {
+  return api.get(`/order/${deskId}`);
+};
+
+export const addOrder = async (seatId, productId, quantity) => {
+  api.post(`/order/${deskId}`, { productId, quantity });
+};
+
+export const checkOutOrder = async (seatId) => {
+  api.patch(`/order/${seatId}`);
+};
